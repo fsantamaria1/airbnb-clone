59c3ea7e83d8bd7c84d077a804cefb43d1a026ba
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-02-28 20:49:35 -0500

    Create the AddPropertyButton component

---
+++ airbnb-clone/app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertButton = () => {
+    return (
+        <div className="custor-pointer p-2 text-sm font-semibold rounded-full hover:bg-gray-200">
+            Airbnb you home
+        </div>
+    )
+}
+
+export default AddPropertButton;
