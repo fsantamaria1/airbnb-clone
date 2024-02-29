53ec7b8105bab0dcc1fe20067bf7b70372ae60ab
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-02-28 21:09:07 -0500

    Import and add categories component

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-    <main className="">
-      Airbnb Clone
+    <main className="max-w-[1500px] mx-auto px-6">
+      <Categories />
     </main>
   );
 }
