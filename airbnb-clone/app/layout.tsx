aff6cb66577481fa9a5137f2b3b066c4a899006a
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-02-29 21:51:55 -0500

    Add Login and Signup Modals to the layout

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 
+import LoginModal from "./components/modals/LoginModal";
+import SignupModal from "./components/modals/SignupModal";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,6 +25,9 @@ export default function RootLayout({
           <div className="pt-32">
             {children}
           </div>
+
+          <LoginModal />
+          <SignupModal />
       </body>
     </html>
   );
