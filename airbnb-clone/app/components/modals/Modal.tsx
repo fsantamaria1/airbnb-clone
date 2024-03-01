181f2d50c46a2f38953384d996b1e2a5bcab83ea
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-02-29 21:09:20 -0500

    Add state and functions to handle close events

--- airbnb-clone/app/components/modals/Modal.tsx
+++ airbnb-clone/app/components/modals/Modal.tsx
@@ -1,18 +1,41 @@
 'use client';
 
+import {useState, useEffect, useCallback} from "react";
+
 interface ModalProps {
     label: string;
     content: React.ReactElement;
+    isOpen: boolean;
+    close: () => void;
 }
 
 const Modal: React.FC<ModalProps> = ({
     label,
-    content
+    content,
+    isOpen,
+    close
 }) => {
+    const [showModal, setShowModal] = useState(isOpen)
+
+    useEffect(() => {
+        setShowModal(isOpen)
+    }, [isOpen])
+
+    const handleClose = useCallback(() => {
+        setShowModal(false);
+
+        setTimeout(() => {
+            close();
+        }, 300)
+    }, [close])
+
+    if (!isOpen) {
+        return null;
+    }
     return (
         <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/60">
             <div className="relative w-[90%] md: w-[80%] lg:w-[700px] my-6 mx-auto h-auto">
-                <div className={`translate duration-600 h-full translate-y-0 opacity-100`}>
+                <div className={`translate duration-600 h-full ${showModal ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-10'}`}>
                     <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
                         <header className="h-[60px] flex items-center p-6 rounded-t justify-center relative border-b">
                             <div className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
