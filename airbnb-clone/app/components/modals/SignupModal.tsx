796d51fa8b9b950a434ed0de96b7cd2435246691
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-03-04 20:31:36 -0500

    refactor: add state to the form fields

--- airbnb-clone/app/components/modals/SignupModal.tsx
+++ airbnb-clone/app/components/modals/SignupModal.tsx
@@ -7,22 +7,33 @@ import CustomButton from "../forms/CustomButton";
 
 
 const SignupModal = () => {
-
     const signupModal = useSignupModal()
+    const [email, setEmail] = useState('');
+    const [errors, seterrors] = useState<String[]>([]);
+    const [password1, setpassword1] = useState('');
+    const [password2, setpassword2] = useState('');
+
 
     const content = (
         <>
             <form className="space-y-4">
-                <input placeholder="Your email" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your email" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
                 
-                <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setpassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <input placeholder="Confirm password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setpassword2(e.target.value)} placeholder="Confirm password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    Error message here
-                </div>
-
+                {errors.map((error, index) => {
+                    return (
+                        <div 
+                            key={`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        >
+                        {error}
+                    </div>
+                    )
+                })}
+                
                 <CustomButton
                     label="Submit"
                     onClick={() => console.log("Clicked")}
