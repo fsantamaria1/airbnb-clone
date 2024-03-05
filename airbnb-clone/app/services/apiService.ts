220eecbe80d0b9dc8b595bab8c9d062bb945e4d3
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-03-04 19:46:48 -0500

    create: post method

--- airbnb-clone/app/services/apiService.ts
+++ airbnb-clone/app/services/apiService.ts
@@ -18,6 +18,28 @@ const apiService = {
                     reject(error);
                 }))
         })
+    },
+
+    post: async function(url: string, data: any): Promise<any> {
+        console.log ('post', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
     }
 }
 
