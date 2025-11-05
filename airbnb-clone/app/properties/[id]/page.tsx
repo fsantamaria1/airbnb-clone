71a4879dbe673dab1f0b552cbabc3c47f8653f2f
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2025-11-05 11:01:33 -0500

    Pass on the props to the ReservationSidebar

--- airbnb-clone/app/properties/[id]/page.tsx
+++ airbnb-clone/app/properties/[id]/page.tsx
@@ -47,7 +47,9 @@ const PropertyDetailPage = async ({params}: { params: {id: string}}) => {
                     </p>
                 </div>
 
-                <ReservationSidebar />
+                <ReservationSidebar 
+                property={property}
+                />
             </div>
 
         </main>
